e1da24e674ad82469f59ec9df97b7942ceaa30bc
VincentLiu <13120200491@163.com>
2021-01-13 11:26:04 +0800

    calculation of all in/out/balance done

--- Liushui/analyzer.py
+++ Liushui/analyzer.py
@@ -5,9 +5,12 @@ import time
 import Modules.mongodb as mongo
 import Modules.public_module as md
 import time
+import datetime
 
 
 def expand_date(df, start_date, end_date):
+    start = datetime.datetime.strptime(start_date, '%Y%m%d').date()
+    end = datetime.datetime.strptime(end_date, '%Y%m%d').date()
     df['date'] = df['交易日期'].apply(str)
     df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
     df = df.loc[:, ('date', '流入金额', '流出金额', '交易后余额')]
@@ -19,21 +22,36 @@ def expand_date(df, start_date, end_date):
     new_df['out'].fillna(value=0, inplace=True)
     for i in df.index:
         date_df = df.loc[i]
+        if isinstance(date_df, pd.DataFrame):   # this step is to cut data exceed the start and end dates
+            date = date_df.index[0]
+            if date < start or date > end:
+                continue
+        else:
+            date = date_df.name
+            if date < start or date > end:
+                continue
         in_sum = date_df['流入金额'].sum()
         out_sum = date_df['流出金额'].sum()
         new_df.loc[i, 'in'] = in_sum
         new_df.loc[i, 'out'] = out_sum
+
     cur_balance = df.iloc[0].loc['交易后余额'] - df.iloc[0].loc['流入金额'] + df.iloc[0].loc['流出金额']
     for i in new_df.index:  # 每天看是否有新的余额更新
         try:
             date_df = df.loc[i]
             if isinstance(date_df, pd.DataFrame):
+                date = date_df.index[0]  # this step is to cut data exceed the start and end dates
+                if date < start or date > end:
+                    continue
                 cur_balance = date_df.iloc[-1].loc['交易后余额']
             else:
+                date = date_df.name
+                if date < start or date > end:
+                    continue
                 cur_balance = date_df.loc['交易后余额']
-            new_df.loc[i, 'balance'] = cur_balance
         except:
-            new_df.loc[i, 'balance'] = cur_balance
+            pass    # no date was found, use previous balance
+        new_df.loc[i, 'balance'] = cur_balance
     # print(df['交易后余额'])
     # print(df[df.index.duplicated()])
     # print(df['交易后余额'].resample('30d').asfreq()[0:5])
@@ -49,9 +67,17 @@ def calculation_A(company, batch_id, file_output):
                                 'Cache')
         cur_df = pd.read_json(data[0]['data'])
         cur_df = expand_date(cur_df, info['start_date'], info['end_date'])
-        print(cur_df)
+        # print(cur_df)
         dfs.append(cur_df)
-
+    all_df = pd.DataFrame(columns=['in', 'out', 'balance'])
+    for df in dfs:
+        all_df = all_df.add(df, fill_value=0)
+    print(all_df)
+    balance_all = all_df['balance']
+    in_all = all_df['in'].resample('1M', label='left', loffset=datetime.timedelta(days=1), closed='left').sum()
+    out_all = all_df['out'].resample('1M', label='left', loffset=datetime.timedelta(days=1), closed='left').sum()
+    print(balance_all, in_all, out_all, sep='\n')
+    return balance_all, in_all, out_all
 
 
 if __name__ == '__main__':
--- Liushui/test.py
+++ Liushui/test.py
@@ -2,5 +2,17 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import time
 
-
+if __name__ == '__main__':
+    start_time = time.time()
+    df1 = pd.DataFrame(np.ones((4, 4)) * 1, columns=list('DCBA'), index=list('4321'))
+    df2 = pd.DataFrame(np.ones((4, 4)) * 2, columns=list('FEDC'), index=list('6543'))
+    df3 = pd.DataFrame(np.ones((4, 4)) * 3, columns=list('DCBA'), index=list('6521'))
+    all_df = pd.DataFrame(columns=['in', 'out', 'balance'])
+    print(df1, df2, df3, sep='\n')
+    df_sum = df1.add(df3, fill_value=0)
+    print(df_sum)
+    s = df_sum.iloc[2]
+    # print(s.name)
+    print('======= Time taken: %f =======' % (time.time() - start_time))
