edbe3c7196f71d3b04865ee5181556bbca708a86
VincentLiu <13120200491@163.com>
2021-01-21 18:34:44 +0800

    modify gitignore

--- Liushui/bank_config.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-
-def db():
-    db = create_engine('mysql+pymysql://bank_dev:072EeAb717e269bF@rm-uf6z3yjw3719s70sbuo.mysql.rds.aliyuncs.com:3306/bank_dev')
-    return db
-
-
-
